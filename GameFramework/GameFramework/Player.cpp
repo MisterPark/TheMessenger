84ec93884544859de9d090af4b2980c6e785c63d
MisterPark <pkh94949494@gmail.com>
2020-07-16 19:50:58 +0900

    일단커밋 집가서하자

--- GameFramework/GameFramework/Player.cpp
+++ GameFramework/GameFramework/Player.cpp
@@ -18,7 +18,7 @@ Player::~Player()
 
 void Player::Update()
 {
-	state = PlayerState::IDLE;
+	PlayerState curState = PlayerState::IDLE;
 
 	if (InputManager::GetKey(VK_UP))
 	{
@@ -32,45 +32,51 @@ void Player::Update()
 	{
 		position.x -= speed * TimeManager::DeltaTime();
 		direction = Direction::LEFT;
-		state = PlayerState::MOVE_LEFT;
+		curState = PlayerState::MOVE_LEFT;
 	}
 	if (InputManager::GetKey(VK_RIGHT))
 	{
 		position.x += speed * TimeManager::DeltaTime();
 		direction = Direction::RIGHT;
-		state = PlayerState::MOVE_RIGHT;
+		curState = PlayerState::MOVE_RIGHT;
 	}
 
 	anim2->SetAnimation(SpriteIndex::NONE);
 
-	switch (state)
+	if (state != curState)
 	{
-	case PlayerState::IDLE:
-		if (direction == Direction::LEFT)
+		state = curState;
+
+		switch (curState)
 		{
-			anim->SetAnimation(SpriteIndex::PLAYER_IDLE_L);
+		case PlayerState::IDLE:
+			if (direction == Direction::LEFT)
+			{
+				anim->SetAnimation(SpriteIndex::PLAYER_IDLE_L);
+			}
+			else
+			{
+				anim->SetAnimation(SpriteIndex::PLAYER_IDLE_R);
+			}
+
+			break;
+		case PlayerState::MOVE_LEFT:
+			anim2->SetAnimation(SpriteIndex::PLAYER_RUN_BODY_L);
+			anim->SetAnimation(SpriteIndex::PLAYER_RUN_LEG_L1, SpriteIndex::PLAYER_RUN_LEG_L4);
+			break;
+		case PlayerState::MOVE_RIGHT:
+			anim2->SetAnimation(SpriteIndex::PLAYER_RUN_BODY_R);
+			anim->SetAnimation(SpriteIndex::PLAYER_RUN_LEG_R1, SpriteIndex::PLAYER_RUN_LEG_R4);
+			break;
+		case PlayerState::JUMP_LEFT:
+			break;
+		case PlayerState::JUMP_RIGHT:
+			break;
+		default:
+			break;
 		}
-		else
-		{
-			anim->SetAnimation(SpriteIndex::PLAYER_IDLE_R);
-		}
-		
-		break;
-	case PlayerState::MOVE_LEFT:
-		anim2->SetAnimation(SpriteIndex::PLAYER_RUN_BODY_L);
-		anim->SetAnimation(SpriteIndex::PLAYER_RUN_LEG_L1, SpriteIndex::PLAYER_RUN_LEG_L4);
-		break;
-	case PlayerState::MOVE_RIGHT:
-		anim2->SetAnimation(SpriteIndex::PLAYER_RUN_BODY_R);
-		anim->SetAnimation(SpriteIndex::PLAYER_RUN_LEG_R1, SpriteIndex::PLAYER_RUN_LEG_R4);
-		break;
-	case PlayerState::JUMP_LEFT:
-		break;
-	case PlayerState::JUMP_RIGHT:
-		break;
-	default:
-		break;
 	}
+	
 	anim->Update();
 	anim2->Update();
 }
